088eba13c19d1ef96235dcf41bfd77efc85695c6
Canut Fernandez <ikermartincanut@gmail.com>
2019-06-11 17:24:25 -0300

    DESESPERACIONAAAA III LA SEGUNDA SECUELA

--- src/Carton.php
+++ src/Carton.php
@@ -7,7 +7,7 @@ class Carton implements CartonInterface {
   private $numeros_carton = [];
 
   public function __construct(array $aux) {
-    $this->numeros_carton = array_map(null, $aux[0], $aux[1], $aux[2], $aux[3], $aux[4], $aux[5], $aux[6], $aux[7], $aux[8]);
+    $this->numeros_carton = $aux;
   }
     
    /**
