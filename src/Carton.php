91e9b181bb2c21ec55372f307859aadb4ac65e0f
Canut Fernandez <ikermartincanut@gmail.com>
2019-06-11 16:29:09 -0300

    Se arregla el Carton

--- src/Carton.php
+++ src/Carton.php
@@ -6,8 +6,7 @@ class Carton implements CartonInterface {
 
   private $numeros_carton = [];
 
-  public function __construct(intentoCarton $intento) {
-    $aux = $intentos->intentoCarton();
+  public function __construct(array $aux) {
     $this->numeros_carton = array_map(null, $aux[0], $aux[1], $aux[2], $aux[3], $aux[4], $aux[5], $aux[6], $aux[7], $aux[8]);
   }
     
@@ -15,28 +14,19 @@ class Carton implements CartonInterface {
    * {@inheritdoc}
    */
   public function filas() {
-    return [
-      [0, 16, 0, 38, 47, 0, 67, 77, 0],
-      [9, 0, 28, 35, 0, 55, 0, 0, 84],
-      [0, 12, 26, 0, 45, 0, 61, 0, 89],
-    ];
+    return $this->numeros_carton;
   }
 
   /**
    * {@inheritdoc}
    */
   public function columnas() {
-    return [
-      [0,9,0],
-      [16,0,12],
-      [0,28,26],
-      [38,35,0],
-      [47,0,45],
-      [0,55,0],
-      [67,0,61],
-      [77,0,0],
-      [0,84,89],
-    ];
+    $cols = [];
+    
+    $filas = $this->filas();
+    $cols = array_map(null, $filas[0], $filas[1], $filas[2]);
+    
+    return $cols;
   }
   
     /**
